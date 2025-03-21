c36f67843d47e3d6ee4e0bcb5127ffae096b4f43
Tomi Joshua <joshua.tomi1@gmail.com>
2025-03-21 10:57:06 +0100

    Took out switch logo and tweaked project img width

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,10 +5,7 @@ export default function Banner() {
     return (
         <section className='banner'>
             <div className='banner-text'>
-                <div className='banner-intro'>
-                    <p>By day, I'm a product designer at</p>
-                    <img src={theSwitch} alt="Interswitch" />
-                </div>
+                <p>By day, I'm a product designer at Interswitch</p>
                 <p>By night, it's a whole other story...</p>
                 <h1>I design, think, tinker and make products with AI</h1>
             </div>
